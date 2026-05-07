a7661a0cd326301603374acf4b62a7bb7169c506
tungluuquang <tungq2005@gmail.com>
2026-05-07 16:29:49 +0700

    chore: set up command-service

--- command-service/src/main/java/org/vippro/command_service/CommandServiceApplication.java
+++ command-service/src/main/java/org/vippro/command_service/CommandServiceApplication.java
@@ -2,8 +2,10 @@ package org.vippro.command_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CommandServiceApplication {
 
 	public static void main(String[] args) {
