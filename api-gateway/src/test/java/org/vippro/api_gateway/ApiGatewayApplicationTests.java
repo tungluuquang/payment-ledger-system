a108e3c052a5e9138ef5ffef0e02027115de2749
tungluuquang <tungq2005@gmail.com>
2026-06-14 15:47:54 +0700

    big-feat: api-gateway and service-discovery

--- api-gateway/src/test/java/org/vippro/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/org/vippro/api_gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,32 @@
 package org.vippro.api_gateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.gateway.route.RouteLocator;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = {
+		"eureka.client.enabled=false",
+		"management.health.eureka.enabled=false"
+})
 class ApiGatewayApplicationTests {
+
+	@Autowired
+	private RouteLocator routeLocator;
 
 	@Test
 	void contextLoads() {
+		assertThat(routeLocator.getRoutes()
+				.map(route -> route.getId())
+				.collectList()
+				.block())
+				.contains(
+						"payment-command",
+						"payment-query",
+						"account-api"
+				);
 	}
 
 }
--- service-discovery/src/test/java/org/vippro/service_discovery/ServiceDiscoveryApplicationTests.java
+++ service-discovery/src/test/java/org/vippro/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -3,7 +3,10 @@ package org.vippro.service_discovery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"eureka.client.register-with-eureka=false",
+		"eureka.client.fetch-registry=false"
+})
 class ServiceDiscoveryApplicationTests {
 
 	@Test
