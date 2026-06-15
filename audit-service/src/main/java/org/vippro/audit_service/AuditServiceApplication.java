7484be94e0405dca830eb8f31a3dc93b64451767
tungluuquang <tungq2005@gmail.com>
2026-06-15 10:34:29 +0700

    feat: init reconciliation, audit and analytics

---
+++ audit-service/src/main/java/org/vippro/audit_service/AuditServiceApplication.java
@@ -0,0 +1,13 @@
+package org.vippro.audit_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditServiceApplication.class, args);
+	}
+
+}
